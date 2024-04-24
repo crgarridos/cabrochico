a00088d0dda964cfdbdda206b8dde091a0c6f843
Cristian Garrido <crgarrido.s@gmail.com>
2024-04-25 00:45:11 +0200

    Change base repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://crgarridos.github.io/cabrochico',
+  base: 'cabrochico',
   integrations: [tailwind()]
 });
