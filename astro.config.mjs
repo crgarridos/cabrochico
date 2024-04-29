02cbfcbb59808c0140547d5bfa9c7822560750fe
Cristian Garrido <crgarrido.s@gmail.com>
2024-04-29 18:30:44 +0200

    Workaround for nav active item

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: 'https://crgarridos.github.io/cabrochico',
   base: 'cabrochico',
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  trailingSlash: "ignore", // explicitly here. we need it for base + active higlighting in Header
 });
