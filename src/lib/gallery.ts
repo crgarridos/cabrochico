e3664b07602d63c1c1e18e7b96251472186f49da
Cristian Garrido <crgarrido.s@gmail.com>
2024-05-30 06:27:46 +0200

    Clean up gallery

--- src/lib/gallery.ts
+++ src/lib/gallery.ts
@@ -1,7 +1,7 @@
 import type { ImageMetadata } from "astro";
 
 export const PHOTOS = import.meta.glob<{ default: ImageMetadata }>(
-  "/src/assets/img/photos/*.{webp,jpeg,jpg,png,gif}"
+  "/src/assets/img/photos/*.{webp,jpeg,jpg,png,gif,JPEG,JPG}"
 );
 
 export const PATHS = Object.keys(PHOTOS);
