dd55f34652f32c53cd22bb882fe2d7c098eea138
Cristian Garrido <crgarrido.s@gmail.com>
2024-09-12 23:20:41 +0200

    List new concert + reverse future ones

--- src/concerts.js
+++ src/concerts.js
@@ -1,166 +1,198 @@
 export const CONCERTS = [
-  {
-      date: "2024-03-23",
-      place: "Barock l'Horloge",
-      city: "Boulogne-sur-mer",
-      bands: ["Naked Soft Men"],
-      source: "https://www.facebook.com/events/423091523582368",
-  },
-  {
-      date: "2023-09-01",
-      place: "Amul Solo",
-      city: "Lille",
-      bands: ["Sea horse shoe"],
-      source: "https://www.facebook.com/events/975097153752804",
-  },
-  {
-      date: "2023-06-23",
-      place: "La planche Aperock #8 - Skatepark Dunkerque",
-      city: "Dunkerque",
-      bands: ["La Frappe", "Veto"],
-      source: "https://www.facebook.com/events/6515821751803513",
-  },
-  {
-      date: "2023-05-06",
-      place: "Amul Solo",
-      city: "Lille",
-      bands: ["Funky Mix"], // TODO, not sure
-      source: "https://www.facebook.com/cabrochicoband/posts/pfbid02iNUfmDsXtVPiKEWJAx4x66DPDcVz3Meg8VB4jZk9TqzPnam8n5srhr3KA6B1MfPGl",
-  },
-  {
-      date: "2023-05-06",
-      place: "Pico Bistrot",
-      city: "Lille",
-      bands: ["Stop Giving Power"],
-      source: "https://www.facebook.com/cabrochicoband/posts/pfbid03zT1XU1UN4yE4y5JSBQPQtaSnc4nhQH3hgDjHvmA9sA6TT3JU77kMopCgza8tsifl",
-  },
-  {
-      date: "2023-04-13",
-      place: "Radio Campus",
-      city: "Lille",
-      source: "https://www.facebook.com/cabrochicoband/posts/pfbid0w6rANgXx2ZFy5tQcCqAhKuPxKDVoevWYLXhVJt8D92D7ZGKJPZAiWEv8LK8baa1dl",
-  },
-  {
-      date: "2022-12-10",
-      place: "Noël à l'APU - Halles aux sucrès",
-      city: "Lille",
-      bands: ["Victor", "Utopie"],
-      source: "https://www.facebook.com/cabrochicoband/posts/pfbid0fNHdMV3s43jPiL5wcq1s9hU76bBs3gQUuW8gQKbKSP1TogcEDDFkfxGnjdRTbQqvl",
-  },
-  {
-      date: "2022-11-26",
-      place: "Festival Culture Bar-bars - Amul Solo",
-      city: "Lille",
-      bands: ["Poncharello", "Wired"],
-      source: "https://www.facebook.com/events/471637028401951/",
-  },
-  {
-      date: "2023-10-06",
-      place: "Festival de cinéma Colombia Migrante - Cinéma l'Univers",
-      city: "Lille",
-      bands: ["Vojabés, que Tin y que Tan"],
-      source: "https://www.facebook.com/events/s/festival-de-cinema-colombia-mi/696951285807323/",
-  },
-  {
-      date: "2022-11-12",
-      place: "Le bus magique",
-      city: "Lille",
-      source: "https://www.facebook.com/events/681375819835245",
-  },
-  {
-      date: "2024-01-13",
-      place: "Festival IGWAP - Houblons-Nous",
-      city: "Lille",
-      bands: ["Pillows", "Ostinato", "The blue waffles"],
-      source: "https://www.facebook.com/events/1084263229412524",
-  },
-  {
-      date: "2022-12-09",
-      place: "Festival IGWAP - La Brat Cave",
-      city: "Lille",
-      bands: ["THE PILLOWS", "ABENDROT", "NAKED SOFT MEN"],
-      source: "https://www.facebook.com/events/582159193911441/",
-  },
-  {
-      date: "2024-06-22",
-      place: "Le Bidule",
-      city: "Lille",
-      bands: ["Poisson De L'aube"],
-  },
-  {
-      date: "2024-04-18",
-      place: "La Griffe",
-      city: "Lille",
-      bands: ["Misskeen", "Fire Life"],
-  },
-  {
-      date: "2024-04-07",
-      place: "Tattoo Hippie Club",
-      city: "Dunkerque",
-      bands: ["Misskeen"],
-  },
-  {
-      date: "2024-03-15",
-      place: "Le Bidule",
-      city: "Lille",
-      bands: ["Misskeen"],
-  },
-  {
-      date: "2024-01-27",
-      place: "Amul Solo",
-      city: "Lille",
-      bands: ["Kenmujo"],
-  },
-  {
-      date: "2023-10-28",
-      place: "Arena Bar",
-      city: "Lille",
-      bands: ["Misskeen"],
-  },
-  {
-      date: "2023-10-14",
-      place: "Amul Solo",
-      city: "Lille",
-      bands: ["Sea Horse Shoe"],
-  },
-  {
-      date: "2023-07-06",
-      place: "Centre Culturel Libertaire",
-      city: "Lille",
-      bands: ["Slowkiss", "Eastern Trade"],
-  },
-  {
-      date: "2023-06-16",
-      place: "Le Distrot",
-      city: "Lille",
-  },
-  {
-      date: "2023-06-11",
-      place: "Yeti Bar",
-      city: "Lille",
-      bands: ["Stuck In Yesterday"],
-      source: "https://www.facebook.com/events/1909633439414041/",
-  },
-  {
-      date: "2022-10-14",
-      place: "Centre Culturel Libertaire",
-      city: "Lille",
-      bands: ["Slowkiss", "Wired"],
-      source: "https://www.facebook.com/events/654583165880514/",
-  },
-  {
-      date: "2022-09-16",
-      place: "Le plateau du Fresnoy",
-      city: "Tourcoing",
-      bands: ["Glory holes & the geniters", "Les vielles bites"],
-      source: "https://www.facebook.com/events/485848579712410/",
-  },
+    {
+        date: "2024-03-23",
+        place: "Barock l'Horloge",
+        city: "Boulogne-sur-mer",
+        bands: ["Naked Soft Men"],
+        source: "https://www.facebook.com/events/423091523582368",
+    },
+    {
+        date: "2023-09-01",
+        place: "Amul Solo",
+        city: "Lille",
+        bands: ["Sea horse shoe"],
+        source: "https://www.facebook.com/events/975097153752804",
+    },
+    {
+        date: "2023-06-23",
+        place: "La planche Aperock #8 - Skatepark Dunkerque",
+        city: "Dunkerque",
+        bands: ["La Frappe", "Veto"],
+        source: "https://www.facebook.com/events/6515821751803513",
+    },
+    {
+        date: "2023-05-06",
+        place: "Amul Solo",
+        city: "Lille",
+        bands: ["Funky Mix"], // TODO, not sure
+        source:
+            "https://www.facebook.com/cabrochicoband/posts/pfbid02iNUfmDsXtVPiKEWJAx4x66DPDcVz3Meg8VB4jZk9TqzPnam8n5srhr3KA6B1MfPGl",
+    },
+    {
+        date: "2023-05-06",
+        place: "Pico Bistrot",
+        city: "Lille",
+        bands: ["Stop Giving Power"],
+        source:
+            "https://www.facebook.com/cabrochicoband/posts/pfbid03zT1XU1UN4yE4y5JSBQPQtaSnc4nhQH3hgDjHvmA9sA6TT3JU77kMopCgza8tsifl",
+    },
+    {
+        date: "2023-04-13",
+        place: "Radio Campus",
+        city: "Lille",
+        source:
+            "https://www.facebook.com/cabrochicoband/posts/pfbid0w6rANgXx2ZFy5tQcCqAhKuPxKDVoevWYLXhVJt8D92D7ZGKJPZAiWEv8LK8baa1dl",
+    },
+    {
+        date: "2022-12-10",
+        place: "Noël à l'APU - Halles aux sucrès",
+        city: "Lille",
+        bands: ["Victor", "Utopie"],
+        source:
+            "https://www.facebook.com/cabrochicoband/posts/pfbid0fNHdMV3s43jPiL5wcq1s9hU76bBs3gQUuW8gQKbKSP1TogcEDDFkfxGnjdRTbQqvl",
+    },
+    {
+        date: "2022-11-26",
+        place: "Festival Culture Bar-bars - Amul Solo",
+        city: "Lille",
+        bands: ["Poncharello", "Wired"],
+        source: "https://www.facebook.com/events/471637028401951/",
+    },
+    {
+        date: "2023-10-06",
+        place: "Festival de cinéma Colombia Migrante - Cinéma l'Univers",
+        city: "Lille",
+        bands: ["Vojabés, que Tin y que Tan"],
+        source:
+            "https://www.facebook.com/events/s/festival-de-cinema-colombia-mi/696951285807323/",
+    },
+    {
+        date: "2022-11-12",
+        place: "Le bus magique",
+        city: "Lille",
+        source: "https://www.facebook.com/events/681375819835245",
+    },
+    {
+        date: "2024-01-13",
+        place: "Festival IGWAP - Houblons-Nous",
+        city: "Lille",
+        bands: ["Pillows", "Ostinato", "The blue waffles"],
+        source: "https://www.facebook.com/events/1084263229412524",
+    },
+    {
+        date: "2022-12-09",
+        place: "Festival IGWAP - La Brat Cave",
+        city: "Lille",
+        bands: ["THE PILLOWS", "ABENDROT", "NAKED SOFT MEN"],
+        source: "https://www.facebook.com/events/582159193911441/",
+    },
+    {
+        date: "2024-06-22",
+        place: "Le Bidule",
+        city: "Lille",
+        bands: ["Poisson De L'aube"],
+    },
+    {
+        date: "2024-04-18",
+        place: "La Griffe",
+        city: "Lille",
+        bands: ["Misskeen", "Fire Life"],
+    },
+    {
+        date: "2024-04-07",
+        place: "Tattoo Hippie Club",
+        city: "Dunkerque",
+        bands: ["Misskeen"],
+    },
+    {
+        date: "2024-03-15",
+        place: "Le Bidule",
+        city: "Lille",
+        bands: ["Misskeen"],
+    },
+    {
+        date: "2024-01-27",
+        place: "Amul Solo",
+        city: "Lille",
+        bands: ["Kenmujo"],
+    },
+    {
+        date: "2024-08-30",
+        place: "Amul Solo",
+        city: "Lille",
+        bands: ["Spon"],
+    },
+    {
+        date: "2024-10-12",
+        place: "Amul Solo",
+        city: "Lille",
+        bands: ["TBD"],
+    },
+    {
+        date: "2024-09-21",
+        place: "PUB 13",
+        city: "Fourmies",
+    },
+    {
+        date: "2024-09-20",
+        place: "Les 3 Auvergnats",
+        city: "Beaumont, Belgique",
+    },
+    {
+        date: "2024-09-28",
+        place: "Tattoo Hippie Club",
+        city: "Dunkerque",
+        bands: ["Misskeen", "The Gash", "Mingawash"],
+    },
+    {
+        date: "2023-10-28",
+        place: "Arena Bar",
+        city: "Lille",
+        bands: ["Misskeen"],
+    },
+    {
+        date: "2023-10-14",
+        place: "Amul Solo",
+        city: "Lille",
+        bands: ["Sea Horse Shoe"],
+    },
+    {
+        date: "2023-07-06",
+        place: "Centre Culturel Libertaire",
+        city: "Lille",
+        bands: ["Slowkiss", "Eastern Trade"],
+    },
+    {
+        date: "2023-06-16",
+        place: "Le Distrot",
+        city: "Lille",
+    },
+    {
+        date: "2023-06-11",
+        place: "Yeti Bar",
+        city: "Lille",
+        bands: ["Stuck In Yesterday"],
+        source: "https://www.facebook.com/events/1909633439414041/",
+    },
+    {
+        date: "2022-10-14",
+        place: "Centre Culturel Libertaire",
+        city: "Lille",
+        bands: ["Slowkiss", "Wired"],
+        source: "https://www.facebook.com/events/654583165880514/",
+    },
+    {
+        date: "2022-09-16",
+        place: "Le plateau du Fresnoy",
+        city: "Tourcoing",
+        bands: ["Glory holes & the geniters", "Les vielles bites"],
+        source: "https://www.facebook.com/events/485848579712410/",
+    },
 ].toSorted((c1, c2) => {
-  const d1 = parseInt(c1.date.replace(/-/g, ""));
-  const d2 = parseInt(c2.date.replace(/-/g, ""));
-  return d2 - d1; // Sorted by date DESC
+    const d1 = parseInt(c1.date.replace(/-/g, ""));
+    const d2 = parseInt(c2.date.replace(/-/g, ""));
+    return d2 - d1; // Sorted by date DESC
 });
-
 
 const NOW = new Date();
 export const FUTURE_CONCERTS = CONCERTS.filter(
